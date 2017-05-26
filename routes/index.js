d5b60e425592a9fce0a3f7e2b6a4e46ab49fd0ca
LivvyWu <lulutt0523@gmail.com>
2017-05-26 10:27:10 +0800

    foodsafty

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+
+    var fs_options = [
+        "CAS優良農產品",
+        "吉園圃安全蔬果",
+        "產銷履歷農產品",
+        "CAS有機農產品",
+        "農產品生產溯源",
+        "國產牛肉溯源",
+        "雞蛋溯源",
+        "豬肉溯源"
+        // "水產品溯源",
+        // "不知啥碗糕"
+    ];
+
+    res.render('index', {title: 'Express',fs_options: fs_options});
+});
+
+module.exports = router;
+
