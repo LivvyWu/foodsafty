433d85c4b4b35bfb156fd0490ba63f6001c27882
LivvyWu <lulutt0523@gmail.com>
2017-05-27 23:28:27 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,15 @@ router.get('/', function (req, res, next) {
 
     var fs_options = [
         "CAS優良農產品",
-        "吉園圃安全蔬果",
         "產銷履歷農產品",
         "CAS有機農產品",
+        "吉園圃安全蔬果",
         "農產品生產溯源",
         "國產牛肉溯源",
         "雞蛋溯源",
-        "豬肉溯源"
-        // "水產品溯源",
-        // "不知啥碗糕"
+        "豬肉溯源",
+        "水產品溯源",
+        "不知啥溯源"
     ];
 
     res.render('index', {title: 'Express',fs_options: fs_options});
